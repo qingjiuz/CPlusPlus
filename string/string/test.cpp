b2e0fd85f1507f5e41d6096ca3fee5a38a3af8bd
qingjiuz <2900105829@qq.com>
2023-09-10 18:46:16 +0800

    string接口的学习

--- string/string/test.cpp
+++ string/string/test.cpp
@@ -134,17 +134,116 @@ using namespace std;
 //	return 0;
 //}
 
+//int main()
+//{
+//	string s1 = "hello world";
+//
+//	s1 += "xxx";
+//	cout << s1 << endl;
+//
+//	s1.append(10, 'y');
+//	cout << s1 << endl;
+//
+//	s1.push_back('!');
+//	cout << s1 << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	string s1 = "hello world";
+//	cout << s1 << endl;
+//
+//	//s1.assign("xxxxxxx");
+//	//cout << s1 << endl;
+//
+//	//s1 = "xxxxxx";
+//	//cout << s1 << endl;
+//
+//	//s1.insert(5, "abcdefg", 3, 2);
+//	//cout << s1 << endl;
+//
+//	//s1.erase(0, 1);
+//	//cout << s1 << endl;
+//
+//	//s1.erase(s1.begin(), s1.end());
+//	//cout << s1 << endl;
+//
+//	s1.replace(5, 1, "xxx");
+//	cout << s1 << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	string url = "https://message.bilibili.com/?spm_id_from=333.1007.0.0#/reply";
+//
+//	string protocol, domain, uri;
+//
+//	size_t pos1 = url.find("://");
+//	if (pos1 != string::npos)
+//	{
+//		protocol = url.substr(0, pos1);
+//	}
+//
+//	size_t pos2 = url.find("/", pos1 + 3);
+//	if (pos2 != string::npos)
+//	{
+//		domain = url.substr(pos1 + 3, pos2 - pos1 - 3);
+//		uri = url.substr(pos2 + 1);
+//	}
+//	cout << protocol << endl;
+//	cout << domain << endl;
+//	cout << uri << endl;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	string s1 = "hello world";
+//
+//	//cout << s1 << endl;			// string类
+//	//cout << s1.c_str() << endl;	// string转成char*
+//
+//	cout << s1 << endl;
+//	size_t found = s1.find_first_of("l");
+//	while (found != string::npos)
+//	{
+//		s1[found] = '*';
+//		found = s1.find_first_of("l", found + 1);
+//	}
+//	cout << s1 << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//string s1 = "hello ";
+//	//string s2 = "world";
+//	//cout << s1 + s2 << endl;
+//
+//	return 0;
+//}
+
 int main()
 {
-	string s1 = "hello world";
+	string s1;
+	string s2;
 
-	s1 += "xxx";
-	cout << s1 << endl;
+	// 正常的cin读取到空格就会停止
+	// 假如输入 “hello world”
+	cin >> s1;
+	cout << s1 << endl;		// hello
+	cin >> s2;
+	cout << s2 << endl;		// world
 
-	s1.append(10, 'y');
-	cout << s1 << endl;
-
-	s1.push_back('!');
+	// 提取一行的字符串，包括空格
+	getline(cin, s1);	// getline(cin, s1, 'o'); --> 最后可以自行选择读取到某个字符停止
 	cout << s1 << endl;
 
 	return 0;
